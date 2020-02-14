29f76ebb14406ab2a2e13e2c9eb0f55a18c54830
sj_rifat <45694548+sjRifat@users.noreply.github.com>
2020-02-14 15:10:36 +0600

    Update 10346 - Peter's Smokes.cpp

--- 10346 - Peter's Smokes.cpp
+++ 10346 - Peter's Smokes.cpp
@@ -1,30 +1,33 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define   	pb 			    push_back
-#define   	nn 			    "\n"
-#define   	ss 			    ' '
-typedef 	long long 		ll;
-typedef 	vector<ll>		vll;
+typedef		long long       ll;
+typedef		vector<ll>      vll;
+#define		pb              push_back
+#define		PB              pop_back
+#define		nn              "\n"
+#define		all(p)          p.begin(),p.end()
+#define		zz(v)           (ll)v.size()
+#define		mms(ar,a)       memset(ar,a,sizeof(ar))
+#define		ss              ' '
+#define 	S(a)            scanf("%lld",&a)
+#define 	SS(a,b)         scanf("%lld %lld",&a,&b)
+#define 	SSS(a,b,c)      scanf("%lld %lld %lld",&a,&b,&c)
+#define		gcd(a,b)        __gcd(a,b)
+#define		lcm(a,b)        (a*b)/gcd(a,b)
+#define		pi              acos(-1.0)
 
-int main()
-{
-    ll a,b,c=0,i,j,k,t,m,n,o,x,y,z,ar[10010];
-    while(cin>>m>>n)
-    {
-        c+=m;
-        x=m/n;
-        c+=x;
-        y=m%n;
-        z=x+y;
-        while(z>=n)
-        {
-            x=z/n;
-            c+=x;
-            y=z%n;
-            z=x+y;
+int main(){
+//    freopen("input.txt","r",stdin);
+    ll a,b,c=0,i,j,t,k,lie,m,n,o,x,y,z,ar[200010];
+    while(SS(n,k)==2){
+        ll sm=n;
+        while(n>=k){
+            x=n%k;
+            n/=k;
+            sm+=n;
+            n+=x;
         }
-        cout<<c<<nn;
-        c=0;
+        printf("%lld\n",sm);
     }
 
     return 0;
